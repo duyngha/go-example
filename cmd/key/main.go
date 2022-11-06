3607c46a5bffdb6e8c11cc12e3c695178161fb23
Duy Nguyen <duyngha@gmail.com>
2022-11-06 13:49:51 +0700

    Render jwt token based on client

--- cmd/key/main.go
+++ cmd/key/main.go
@@ -11,13 +11,13 @@ func main() {
 
 	client := models.Client{
 		Name:   "example.com",
-		Key:    "123",
+		KeyID:  "123",
 		Secret: "abc",
 	}
 
 	models.DB.Create(&client)
 	log.Printf("Client created!")
 	log.Printf("Client name: %s", client.Name)
-	log.Printf("Client key: %s", client.Key)
+	log.Printf("Client key: %s", client.KeyID)
 	log.Printf("Client secret: %s", client.Secret)
 }
--- models/client.go
+++ models/client.go
@@ -5,7 +5,7 @@ import "time"
 type Client struct {
 	ID        uint      `json:"id"`
 	Name      string    `json:"name"`
-	Key       string    `json:"key"`
+	KeyID     string    `json:"key_id"`
 	Secret    string    `json:"secret"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
